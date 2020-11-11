ad1bee2ace88ce362f3c7cde4f91cb2848d56ee7
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-11 01:13:09 -0500

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -1,3 +1,60 @@
+var drawscatterPlot = function(classData)
+    var svg = d3.select("#actualgraph1")
+    .append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+{ d3.select("svg")
+    var circles = svg.selectAll("circle")
+                .data(classData)
+                .enter()
+                .append("circle");
+        circles.attr("cx", function(classData)
+              {
+    return getHomeworkgrade(classData);
+}
+              )
+         .attr("cy", function(classData)
+              {
+    return getFinalgrade(classData);
+}
+              )
+         .attr("r", 5)};
+
+var drawScatterplot2 = function(classData)
+   var svg = d3.select("#actualgraph2")
+    .append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+{ d3.select("svg")
+    var circles = svg.selectAll("circle")
+                .data(classData)
+                .enter()
+                .append("circle");
+        circles.attr("cx", function(classData)
+              {
+    return getHomeworkgrade(classData);
+}
+              )
+         .attr("cy", function(classData)
+              {
+    return getQuizgrade(classData);
+}
+              )
+         .attr("r", 5)};
+
+var retrievePromise = d3.json("classData.json");
+var successFCN = function(classData) {
+    console.log("data", classData);
+    drawScatterplot(classData);
+    drawScatterplot2(classData);
+}
+
+
+var failFCN = function(errMessage) {
+    console.log("failure", errMessage);
+}
+retrievePromise.then(successFCN, failFCN);
+
 var getGrade = function(item){
             return item.grade;
         };
@@ -11,46 +68,48 @@ var getGrade = function(item){
             var homework = student.homework;
             var homeworkScores = homework.map(getGrade);
             var avg = d3.mean(finalScores);
+                return avg;};
+            var getQuizgrade = function(student){
+                var quiz = student.homework;
+                var quizScores = quiz.map(getGrade);
+                var avg = d3.mean(finalScores);
                 return avg;
-     var scale = d3.scalelinear()
-            .domain([10,490])
-            .range([10,390]);
-                
-                d3.select("body")
-    .selectall("p")
-    .data(classData)
-    .enter()
-    .append("p")
-    .text(getHomeworkgrade);
+            };
+     var xscale = d3.scalelinear()
+            .domain(10,490)
+            .range(10,390);
+    var yscale = d3.scalelinear()
+            .domain(10,490)
+            .range(10,390);
+
+    var getImage = function(item)
+    {
+        return item.picture;
+    }
+    d3.select("svg")
+.on("mouseover", function(d){
+    var xposition = parseFloat(d3.select(this).attr("cx"));
+    parseFloat(d3.select(this).attr("cy"));
     
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", 400)
-    .attr("height", 400)
-var scatterPlot = d3.select("svg")
-    svg.selectAll("circle")
-                .data(dataset)
-                .enter()
-                .append("circle")
-                .attr("cx", function(classData)
-              {
-    return getHomeworkgrade;
-}
-              )
-         .attr("cy", function(classData)
-              {
-    return getFinalgrade;
-}
-              )
-         .attr("r", 5);
-    var scatterplotPromise = d3.json("classData.json");
-var successFCN = function(classData) {
-    console.log("data", classData);
-    svg(classData);
-    scatterPlot(classData);
-}
-
-
-var failFCN = function(errMessage) {
-    console.log("failure", errMessage);
-}
+svg.append("img")
+    .attr("id", "tooltip")
+    .attr("x", xposition)
+    .attr("y", yposition)
+    getImage(classData);
+            });             
+.on("mouseout", function(){
+    d3.select("#tooltip").remove();
+})
+    d3.select("graph1")
+    .on("click",function())
+    .selectall("#actualgraph1")
+    .remove("#actualgraph1")
+    .selectall("#actualgraph2")
+    .append("#actualgraph2")
+                
+    d3.select("graph2")
+    .on("click",function())
+    .selectall("#actualgraph2")
+    .remove("#actualgraph2")
+    .selectall("#actualgraph1")
+    .append("#actualgraph1")
